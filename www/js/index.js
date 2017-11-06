97f99b1fc04bc8fcc61b1e13877efaf3f55c05d2
Joel <joel.light.years@gmail.com>
2017-11-06 10:44:33 +0000

    Code Change

--- www/js/index.js
+++ www/js/index.js
@@ -1,15 +1,18 @@
+<!-- For more info on jQuery Mobile,  touch gestures and other useful events see : http://api.jquerymobile.com/category/events/ -->
+
 $(document).on("pagecreate","#pageone",function(){
-  $('#submitButton').on("click", function(){
-    submitText();
-  });            
-});            
+  
+  	$('#tapholdtext').on("taphold",function(){
+    	$(this).hide();
+ 	});                       
+
+	$('#taptext').on("tap",function(){
+    	alert("Stop tapping!");
+ 	}); 
+
+	$('#swipetext').on("swipeleft",function(){
+    	$(this).css('color', 'green');
+  	});   
 
 
-function submitText() {
-	var text = $('#textinput').val();
-	alert(text);
-}
-
-function storeValue(key, value) {
-	//add some code to store the key-value pair in persistant storage 
-}
+});
