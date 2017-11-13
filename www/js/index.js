cf50f22b6c8f36b7e0e06869f84ec495055c4b37
Joel <joel.light.years@gmail.com>
2017-11-13 11:01:55 +0000

    w

--- www/js/index.js
+++ www/js/index.js
@@ -1,10 +1,36 @@
 
 
 document.addEventListener("deviceready", onDeviceReady, false);
+
 function onDeviceReady() {
     console.log(navigator.notification);
+    
+     alert("alert test");
+
+//    navigator.notification.beep(1);
+    
+	$('#taptext').on("tap",function(){
+        
+        var randBool = random()
+  
+       $("#taptext").html(randBool.toString());
+        
+        if (randBool == true) {
+            
+            alert("true");
+            navigator.notification.beep(1);
+            
+            } else {
+               alert("false");
+            navigator.nostification.beep(2);
+                
+        }
+        
+ 	}); 
+    
 }
 
+/*
 $(document).on("pagecreate","#pageone",function(){                 
     
     alert("alert test");
@@ -35,3 +61,4 @@ $(document).on("pagecreate","#pageone",function(){
 function random() {
 	return !Math.round(Math.random());
 }
+*/
